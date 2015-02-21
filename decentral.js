bbdbecc130ab049f85bc77589ab48c9888b8ce04
Eric Martindale <eric@ericmartindale.com>
2015-02-20 22:18:36 -0800

    generate a hash per upload

--- decentral.js
+++ decentral.js
@@ -7,6 +7,7 @@ var Schema = mongoose.Schema;
 var ObjectId = Schema.Types.ObjectId;
 
 var Torrent = require('node-torrent-stream');
+var crypto = require('crypto');
 
 var Credit = decentral.define('Credit', {
   internal: true,
@@ -40,13 +41,32 @@ var Recording = decentral.define('Recording', {
   attributes: {
     _show:    { type: ObjectId , ref: 'Show', required: true , alias: 'show' },
     title:    { type: String , max: 35 , required: true , slug: true },
-    audio:    { type: 'File' , required: true },
+    audio:    { type: 'File' },
     recorded: { type: Date },
     released: { type: Date , default: Date.now , required: true },
     description: { type: String },
+    hash: { type: String , max: 32 },
     credits:  [ Credit.Schema ]
   },
   icon: 'sound'
+});
+
+Recording.on('file', function(file) {
+  var recording = this;
+  console.log('hello, file');
+  
+  var shasum = crypto.createHash('md5');
+  file.on('data', function(d) {
+    console.log('data event', d );
+    shasum.update(d);
+  });
+  
+  file.on('end', function() {
+    var d = shasum.digest('hex');
+    console.log('done! md5: ' , d );
+    recording.hash = d;
+  });
+  
 });
 
 var Person = decentral.define('Person', {
